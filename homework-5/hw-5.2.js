46f273842d484424b616962d563aa82e4bd5f5c6
mariyadziuba <158283138+mariyadziuba@users.noreply.github.com>
2024-07-06 13:29:20 +0200

    changed 5.2

--- homework-5/hw-5.2.js
+++ homework-5/hw-5.2.js
@@ -1,8 +1,10 @@
 //Один долар коштує 26 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів
 
 const dollar = 26;
+const maxRate = 100;
+const rateStep = 10;
 
-for (let h = 10; h <=100; h+=10) {
+for (let h = 10; h <= maxRate; h+=rateStep) {
 		const howMuch = dollar * h;
-    console.log(`${h} hrywnas is equilatte for ${howMuch} dollars`);
+    console.log(`${h} uah is equilatte for ${howMuch} dollars`);
 }
