ed8b235bf75a3e57ea073f4752050054796f6f43
mariyadziuba <158283138+mariyadziuba@users.noreply.github.com>
2024-07-27 17:58:49 +0200

    Update hw-10.2.js

--- homework-10/hw-10.2.js
+++ homework-10/hw-10.2.js
@@ -2,9 +2,9 @@
 // Використовуйте вже існуючі методи масиву для створення нового масиву, в якому лише парні числа з оригінального масиву.
 
 
-const arrayOfNumbers = [];
+let arrayOfNumbers = [];
 
-const evenNumbers = arrayOfNumbers.filter((number) => number % 2 === 0);
+let evenNumbers = arrayOfNumbers.filter((number) => number % 2 === 0);
 
 console.log(evenNumbers);
 
