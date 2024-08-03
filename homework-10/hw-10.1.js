f0dc0bb82563be7cec545cb64862b2cac3899fa0
mariyadziuba <158283138+mariyadziuba@users.noreply.github.com>
2024-08-03 13:01:26 +0200

    added hw-10

--- homework-10/hw-10.1.js
+++ homework-10/hw-10.1.js
@@ -1,2 +1,23 @@
 // Створіть об'єкт, що містить інформацію про користувача, таку як ім'я, вік, місце проживання тощо. 
 // Створіть метод об'єкту для отримання та відображення цих даних.
+
+const user = {
+    name: 'Maria',
+    age: 27,
+    city: 'London',
+};
+
+// метод Object.keys() повертає масив ключів
+
+const keys = Object.keys(user);
+console.log(keys);
+
+// метод Object.values() повертає масив значень 
+
+const values = Object.values(user);
+console.log(values);
+
+// метод Object.entries() повертає масив пар ключ-значення
+
+const entries = Object.entries(user);
+console.log(entries);
--- homework-10/hw-10.3.js
+++ homework-10/hw-10.3.js
@@ -1,3 +1,21 @@
 // Створіть об'єкт, який матиме одну властивість з масивом об'єктів. 
 // Які представляють контакти у вашій контактній книзі. Кожен об'єкт має містити ім'я, номер телефону та адресу електронної пошти. 
 // Додайте метод для пошуку контакту за ім'ям та метод для додавання нових контактів.
+
+const phoneBook = {
+    contacts: [
+        {name: 'Maria L', phone: '111-111-111', email: 'maria@mail.com'},
+        {name: 'Julia N', phone: '000-111-000', email: 'julia@mail.com'},
+        {name: 'Petro K', phone: '000-222-000', email: 'petro@mail.com'}
+    ]
+};
+console.log(phoneBook.contacts);
+
+// метод для пошуку контактів за ім'ям
+
+const nameFromContacts = phoneBook.contacts.map(contact => contact.name);
+console.log(nameFromContacts);
+
+//метод для додавання нових контактів.
+
+phoneBook.contacts.push();
